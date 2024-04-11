df66918234ecea17764690c662b2bdebd41465ef
imf22 <92887508+imf22@users.noreply.github.com>
2024-04-10 20:08:10 -0400

    Paritally Imlemented calculateWinner - Check contents of each line for a win - Add return value Paritally implemented validateCoordinates function - Add transfromation matricies for diagonal win lines

--- src/App.js
+++ src/App.js
@@ -13,13 +13,20 @@ function BoardRow({squares, cells, onSquareClick}){
     
     return (
         <div className='board-row'>
-            <Square value={squares[cells[0]]} onSquareClick={() => onSquareClick(cells[0])} />
+            {/* <Square value={squares[cells[0]]} onSquareClick={() => onSquareClick(cells[0])} />
             <Square value={squares[cells[1]]} onSquareClick={() => onSquareClick(cells[1])} />
             <Square value={squares[cells[2]]} onSquareClick={() => onSquareClick(cells[2])} />
             <Square value={squares[cells[3]]} onSquareClick={() => onSquareClick(cells[3])} />
             <Square value={squares[cells[4]]} onSquareClick={() => onSquareClick(cells[4])} />
             <Square value={squares[cells[5]]} onSquareClick={() => onSquareClick(cells[5])} />
-            <Square value={squares[cells[6]]} onSquareClick={() => onSquareClick(cells[6])} />
+            <Square value={squares[cells[6]]} onSquareClick={() => onSquareClick(cells[6])} /> */}
+            <Square value={cells[0]} onSquareClick={() => onSquareClick(cells[0])} />
+            <Square value={cells[1]} onSquareClick={() => onSquareClick(cells[1])} />
+            <Square value={cells[2]} onSquareClick={() => onSquareClick(cells[2])} />
+            <Square value={cells[3]} onSquareClick={() => onSquareClick(cells[3])} />
+            <Square value={cells[4]} onSquareClick={() => onSquareClick(cells[4])} />
+            <Square value={cells[5]} onSquareClick={() => onSquareClick(cells[5])} />
+            <Square value={cells[6]} onSquareClick={() => onSquareClick(cells[6])} />
         </div>
     );
 }
@@ -37,20 +44,21 @@ function Board(){
 
         if (squares[columnNum]) return;
 
+        let nextOpenSquare = getNextSquareInColumn(columnNum);
+
         const nextSquares = squares.slice();
-        nextSquares[getNextSquareInColumn(columnNum)] = isRNext? "R" : "B"; 
+        nextSquares[nextOpenSquare] = isRNext? "R" : "B"; 
         setSquares(nextSquares) ;
         setCurrentMove(currentMove + 1);
+
+        calculateWinner(columnNum, nextOpenSquare, squares);
     }
 
 
 
-    function getNextSquareInColumn(c)
-    {
+    function getNextSquareInColumn(c){
         for (let i=c ; i<35; i){
-            console.log(`On ${i} checking if ${i+7} is empty`);
             if (squares[i+7]){
-                console.log(`   ${i+7} exist!`);
                 return i;  
             }
             i += 7;
@@ -79,4 +87,104 @@ export default function Game(){
             </div>
         </div>
     );
+}
+
+const board2D = [
+    [ 0, 1, 2, 3, 4, 5, 6],
+    [ 7, 8, 9,10,11,12,13],
+    [14,15,16,17,18,19,20],
+    [21,22,23,24,25,26,27],
+    [28,29,30,31,32,33,34],
+    [35,36,37,38,39,40,41]
+];
+
+
+function calculateWinner(c, linearIndex, squares){
+    //           c  : column 
+    //           r  : row
+    // linearIndex  : used to calculate which row cell is in from 1D board array;
+    let r = (linearIndex - c) % 6;
+    console.log("calculateWinner:")
+    console.log(`   Square ${linearIndex} is at ${[c,r]}. Square contains ${board2D[r][c]}`)
+    
+    const lines = [
+        // Vertial Win States
+        [[0, 0], [0, 1], [0, 2], [0, 3]],
+        [[0,-1], [0, 0], [0, 1], [0, 2]],
+        [[0, 2], [0,-1], [0, 0], [0, 1]],
+        [[0,-3], [0,-2], [0,-1], [0, 0]],
+        // Horizontal Win States
+        [[0, 0], [ 1,0], [ 2,0], [ 3,0]],
+        [[-1,0], [ 0,0], [ 1,0], [ 2,0]],
+        [[ 2,0], [-1,0], [ 0,0], [ 1,0]],
+        [[-3,0], [-2,0], [-1,0], [ 0,0]],
+        // Diagonal Decreasing Win States
+        // [[0,1], [0,2], [0,2]],
+        // [[0,1], [0,2], [0,2]],
+        // [[0,1], [0,2], [0,2]],
+        // [[0,1], [0,2], [0,2]],
+        // // Diagonal Increasing Win States
+        // [[0,1], [0,2], [0,2]],
+        // [[0,1], [0,2], [0,2]],
+        // [[0,1], [0,2], [0,2]],
+        // [[0,1], [0,2], [0,2]],
+        
+    ];
+    
+    // Loop through each possible win 
+    // condition including the most recently placed token
+    for (var i=0; i < lines.length; i++){
+        if (validateCoordniates(i, c, r, lines[i])){
+            var [t0, t1, t2, t3]= lines[i];
+            var p1, p2, p3, p4;
+            // p1 = squares[board2D[c+t0[0],r+t0[1]]];
+            p1 = board2D[r+t0[1]][c+t0[0]];
+            p2 = board2D[r+t1[1]][c+t1[0]];
+            p3 = board2D[r+t2[1]][c+t2[0]];
+            p4 = board2D[r+t3[1]][c+t3[0]];
+             // Transform Coordinates
+
+            console.log(`               calculateWinner:\n     Checking... \n      ${[c+t0[0],r+t0[1]]}, ${[c+t1[0],r+t1[1]]}, ${[c+t2[0],r+t2[1]]}, ${[c+t3[0],r+t3[1]]}`)
+            console.log(`calculateWinner: ${t0}`)
+            console.log(`calculateWinner: ${[p1,p2,p3,p4]}`)
+
+
+
+
+        };
+    }
+    
+    
+    
+    return;
+}
+const [bWidthd, bHieght] = [7,6]; 
+
+function validateCoordniates(i,c,r, transforms){
+    const comments = true;
+    if (comments) {console.log(`       coordinateValidator: Checking line ${i} ${transforms} ...`)};
+    for (var i = 0, l = transforms.length; i < l; i++){
+        var cT = transforms[i][0] + c; // c transformed
+        var rT = transforms[i][1] + r; // r transformed
+
+
+        // Check Veritical bounds
+        // if (cT < 0 || cT >= bHieght) return false;
+        if (cT < 0 || cT >= bWidthd) {
+            if (comments) {console.log(`           coordinateValidator: ${[cT, rT]} fails: column out of bounds`)};
+            return false;
+        }
+        
+        // Check horizontal bounds
+        // if (rT < 0 || rT >= bWidthd) return false;
+        if (rT < 0 || rT >= bHieght){
+            if (comments) {console.log(`           coordinateValidator: ${[cT, rT]} fails: row out of bounds`)};
+            return false;
+        } 
+
+
+        if (comments) {console.log(`           coordinateValidator: ${[cT, rT]} is pass`)};
+    }
+    if (comments) {console.log(`         coordinateValidator: ${transforms} is totally valid`)};
+    return true;
 }
